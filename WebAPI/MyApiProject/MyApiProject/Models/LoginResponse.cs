297114b489fb0c137138217390d8230aeb51fdd4
vtrung30211fpt <trungnvswh00499@fpt.edu.vn>
2025-04-16 11:45:17 +0700

    Đẩy code web API lên

---
+++ WebAPI/MyApiProject/MyApiProject/Models/LoginResponse.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MyApiProject.Models
+{
+    public class TokenData
+    {
+        [JsonPropertyName("accessToken")]
+        public string AccessToken { get; set; }
+
+        [JsonPropertyName("refresh_token")]
+        public string RefreshToken { get; set; }
+
+        [JsonPropertyName("tokenType")]
+        public string TokenType { get; set; }
+
+        [JsonPropertyName("expires_in")]
+        public string ExpiresIn { get; set; }
+
+        [JsonPropertyName("refresh_expires_in")]
+        public string RefreshExpiresIn { get; set; }
+    }
+
+    public class TokenResponse
+    {
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
+
+        [JsonPropertyName("message")]
+        public string Message { get; set; }
+
+        [JsonPropertyName("data")]
+        public TokenData Data { get; set; }
+    }
+}
