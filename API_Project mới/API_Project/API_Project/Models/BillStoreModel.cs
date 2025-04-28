758a3537341bb6c23d115a4f37a996392ec56c3d
NguyenVietTrung111203 <vtrung44888888@gmail.com>
2025-04-28 16:53:30 +0700

    đã tạo được API trung gian cùng các endpoint, đã gọi được các API bên dịch vụ, đã tạo được frontend gọi được các API đó

--- API_Project mới/API_Project/API_Project/Models/BillStoreModel.cs
+++ API_Project mới/API_Project/API_Project/Models/BillStoreModel.cs
@@ -1,24 +1,30 @@
-//using System.Text.Json.Serialization;
+using System.Text.Json.Serialization;
 
-//namespace API_Project.Models
-//{
-//	public class TokenData
-//	{
-//		[JsonPropertyName("access_token")]
-//		public string AccessToken { get; set; } = string.Empty;
+namespace API_Project.Models
+{
+	public class TokenData
+	{
+		[JsonPropertyName("access_token")]
+		public string AccessToken { get; set; } = string.Empty;
 
-//		[JsonPropertyName("expires_in")]
-//		//public string ExpiresIn { get; set; }
-//		public int ExpiresIn { get; set; } = int.MinValue;
+		[JsonPropertyName("expires_in")]
+		//public string ExpiresIn { get; set; }
+		public int ExpiresIn { get; set; } = int.MinValue;
 
-//		[JsonPropertyName("refresh_expires_in")]
-//		//public string RefreshExpiresIn { get; set; }
-//		public int RefreshExpiresIn { get; set; } = int.MinValue;
+		[JsonPropertyName("refresh_expires_in")]
+		//public string RefreshExpiresIn { get; set; }
+		public int RefreshExpiresIn { get; set; } = int.MinValue;
 
-//		[JsonPropertyName("refresh_token")]
-//		public string RefreshToken { get; set; } = string.Empty;
+		[JsonPropertyName("refresh_token")]
+		public string RefreshToken { get; set; } = string.Empty;
 
-//		[JsonPropertyName("token_type")]
-//		public string TokenType { get; set; } = string.Empty;
-//	}
-//}
+		[JsonPropertyName("token_type")]
+		public string TokenType { get; set; } = string.Empty;
+	}
+
+	public class TokenResponse
+	{
+		public string AccessToken { get; set; }
+		public string RefreshToken { get; set; }
+	}
+}
--- API_Project mới/API_Project/API_Project/Program.cs
+++ API_Project mới/API_Project/API_Project/Program.cs
@@ -34,9 +34,6 @@
 
 //app.Run();
 
-
-
-
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -44,27 +41,65 @@ using Microsoft.Extensions.Configuration;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
+using Microsoft.AspNetCore.Authentication.BearerToken;
+using API_Project.Models;
+using System.IdentityModel.Tokens.Jwt;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost")  // Đảm bảo rằng frontend của bạn đang chạy trên URL này
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowAnyOrigin();
+    });
+});
+
+
 
 // Đăng ký HttpClient
 builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
+
+app.UseCors("AllowFrontend");
+
 // Endpoint 1: Lấy Access Token
 app.MapPost("/api/token", async (HttpContext context, IHttpClientFactory httpClientFactory) =>
 {   var client = httpClientFactory.CreateClient();
+
+    string? accessToken = context.Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+    string? refreshToken = context.Request.Headers["Authorization"].ToString();
+
+    //if (IsAccessTokenExpired(accessToken))
+    //{
+    //    var refreshedTokens = await RefreshAccessToken(client, refreshToken);
+
+    //    if (refreshedTokens != null)
+    //    {
+    //        accessToken = refreshedTokens.Value.AccessToken;
+    //        refreshToken = refreshedTokens.Value.RefreshToken;
+
+    //        context.Response.Headers.Add("New-Access-Token", accessToken);
+    //        context.Response.Headers.Add("New-Refresh-Token", refreshToken);
+    //    }
+    //    else
+    //    {
+    //        context.Response.StatusCode = 401;
+    //        await context.Response.WriteAsync("Đã hết phiên. Vui lòng đăng nhập lại");
+    //        return;
+    //    }
+    //}
 
     Dictionary<string, string>? requestBody = null;
 
 
     var config = app.Configuration;
-    //var requestBody = await JsonSerializer.DeserializeAsync<Dictionary<string, string>>(context.Request.Body);
-    var username = requestBody?.GetValueOrDefault("username") ?? config["Login:Username"];
-    var password = requestBody?.GetValueOrDefault("password") ?? config["Login:Password"];
-
-    
 
     try
     {
@@ -72,8 +107,15 @@ app.MapPost("/api/token", async (HttpContext context, IHttpClientFactory httpCli
     }
     catch
     {
-
+        context.Response.StatusCode = 400; //Bad request
+        await context.Response.WriteAsync("Request body format không hợp lệ");
+        return;
     }
+
+
+    //var requestBody = await JsonSerializer.DeserializeAsync<Dictionary<string, string>>(context.Request.Body);
+    var username = requestBody?.GetValueOrDefault("username") ?? config["Login:Username"];
+    var password = requestBody?.GetValueOrDefault("password") ?? config["Login:Password"];
 
     var apiBody = new
     {
@@ -85,30 +127,116 @@ app.MapPost("/api/token", async (HttpContext context, IHttpClientFactory httpCli
 
     var response = await client.PostAsync("https://stg-accounts-api.xcyber.vn/management/cyberid/login", content);
     var responseContent = await response.Content.ReadAsStringAsync();
+    context.Response.ContentType = "application/json";
 
-    context.Response.StatusCode = (int)response.StatusCode;
-    await context.Response.WriteAsync(responseContent);
+
+    if (response.IsSuccessStatusCode)
+    {
+        context.Response.StatusCode = (int)response.StatusCode;
+        await context.Response.WriteAsync(responseContent);
+    }
+    else
+    {
+        context.Response.StatusCode = (int)response.StatusCode;
+        await context.Response.WriteAsync("Authentication không thành công");
+    }
 });
 
-// Endpoint 2: Lấy Invoice List
-app.MapGet("/api/invoices", async (HttpContext context, IHttpClientFactory httpClientFactory) =>
+
+//bool IsAccessTokenExpired(string accessToken)
+//{
+//    var tokenHandler = new JwtSecurityTokenHandler();
+//    var token = tokenHandler.ReadJwtToken(accessToken);
+//    var expriy = token.ValidTo;
+//    return expriy < DateTime.UtcNow;
+//}
+
+
+//async Task<(string AccessToken, string RefreshToken)?> RefreshAccessToken(HttpClient client, string refreshToken)
+//{
+//    var refreshRequestBody = new
+//    {
+//        refresh_token = refreshToken
+//    };
+
+//    var refreshContent = new StringContent(JsonSerializer.Serialize(refreshRequestBody), Encoding.UTF8, "application/json");
+
+//    var response = await client.PostAsync("https://stg-accounts-api.xcyber.vn/management/cyberid/refresh-token", refreshContent);
+
+//    if (response.IsSuccessStatusCode)
+//    {
+//        var responseContent = await response.Content.ReadAsStringAsync();
+//        var tokens = JsonSerializer.Deserialize<TokenResponse>(responseContent);
+
+//        if (tokens != null)
+//        {
+//            return (tokens.AccessToken, tokens.RefreshToken);
+//        }
+//    }
+
+//    return null; // Không thể refresh token, yêu cầu người dùng đăng nhập lại
+//}
+
+
+//Endpoint 2: Lấy User Info
+app.MapGet("/api/get-user-info", async (HttpContext context, IHttpClientFactory httpClientFactory) =>
 {
     var client = httpClientFactory.CreateClient();
+    var authorizationHeader = context.Request.Headers["Authorization"].FirstOrDefault();
+    var token = authorizationHeader?.Replace("Bearer ", "");
 
-    var token = context.Request.Headers["token"].FirstOrDefault();
-    var doanhnghiepma = context.Request.Headers["doanhnghiepma"].FirstOrDefault() ?? "";
-    var userma = context.Request.Headers["userma"].FirstOrDefault() ?? "";
-
-    var requestMessage = new HttpRequestMessage(HttpMethod.Get, "https://dev-billstore.xcyber.vn/api/hddv-hoa-don/get-list?current=1&page=0&pageSize=10&size=10&trangThaiPheDuyet");
-    requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
-    requestMessage.Headers.Add("doanhnghiepma", doanhnghiepma);
-    requestMessage.Headers.Add("userma", userma);
+    var requestMessage = new HttpRequestMessage(HttpMethod.Get, "https://dev-billstore.xcyber.vn/api/hddv/nguoidung/get-user-info");
+    if (!string.IsNullOrEmpty(token))
+    {
+        requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+    }
 
     var response = await client.SendAsync(requestMessage);
     var responseContent = await response.Content.ReadAsStringAsync();
 
     context.Response.StatusCode = (int)response.StatusCode;
+    context.Response.ContentType = "application/json";
     await context.Response.WriteAsync(responseContent);
+});
+
+//Endpoint 3: Lấy Invoice List
+app.MapGet("/api/invoices", async (HttpContext context, IHttpClientFactory httpClientFactory) =>
+{
+    var client = httpClientFactory.CreateClient();
+    var authorizationHeader = context.Request.Headers["Authorization"].FirstOrDefault();
+    var token = authorizationHeader?.Replace("Bearer ", "");
+
+    var doanhnghiepma = context.Request.Headers["doanhnghiepma"].FirstOrDefault() ?? string.Empty;
+    var userma = context.Request.Headers["userma"].FirstOrDefault() ?? string.Empty;
+
+    var requestMessage = new HttpRequestMessage(HttpMethod.Get, "https://dev-billstore.xcyber.vn/api/hddv-hoa-don/get-list?current=1&page=0&pageSize=10&size=10&trangThaiPheDuyet");
+
+    if (!string.IsNullOrEmpty(token))
+    {
+        requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+    }
+    if (!string.IsNullOrEmpty(doanhnghiepma))
+    {
+        requestMessage.Headers.Add("doanhnghiepma", doanhnghiepma);
+    }
+    if (!string.IsNullOrEmpty(userma))
+    {
+        requestMessage.Headers.Add("userma", userma);
+    }
+
+
+    var response = await client.SendAsync(requestMessage);
+    var responseContent = await response.Content.ReadAsStringAsync();
+
+    context.Response.StatusCode = (int)response.StatusCode;
+    context.Response.ContentType = "application/json";
+    await context.Response.WriteAsync(responseContent);
+
+
+    Console.WriteLine($"Token: {token}");
+    Console.WriteLine($"DoanhNghiepMa: {doanhnghiepma}");
+    Console.WriteLine($"UserMa: {userma}");
+
 });
 
 app.Run();
