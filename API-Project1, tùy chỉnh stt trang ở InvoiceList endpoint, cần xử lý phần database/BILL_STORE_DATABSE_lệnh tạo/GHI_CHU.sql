d4a090a892a625bb6f77ddac60697f8442ab77bf
NguyenVietTrung111203 <vtrung44888888@gmail.com>
2025-05-21 23:28:01 +0700

    adewfvgfd

---
+++ API-Project1, tùy chỉnh stt trang ở InvoiceList endpoint, cần xử lý phần database/BILL_STORE_DATABSE_lệnh tạo/GHI_CHU.sql
@@ -0,0 +1,9 @@
+CREATE TABLE GHI_CHU (
+    ghiChu_id INT NOT NULL,
+    checkTrangThaiXuLy INT NOT NULL,
+    checkTrangThaiHoaDon INT NOT NULL,
+    checkTenNguoiMua INT NULL,
+    checkDiaChiNguoiMua INT NULL,
+    checkMstNguoiMua INT NULL,
+    checkHoaDonKyDienTu INT NULL
+);
